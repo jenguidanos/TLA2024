144fea31615bbe58cbd93f113abd15116d7261e1
mau-guerrero <smg10@rice.edu>
2018-08-10 11:10:35 -0500

    adds setMux() with limited options.

--- lib/TLA2024/TLA2024.h
+++ lib/TLA2024/TLA2024.h
@@ -9,7 +9,29 @@ class TLA2024 {
 
   float analogRead();
 
+  /*
+    Sets the Full Scale Range of the ADC
+
+    gain:
+      60 -> ± 6.144v
+      40 -> ± 4.096
+      20 -> ± 2.048 (default)
+      10 -> ± 1.024
+      5  -> ± 0.512
+      2  -> ± 0.256
+
+  */
   void setFSR(uint8_t gain);
+
+  /*
+    Configures the input signals to the ADC
+
+    option:
+      1 -> P = 0, N = 1 (TP1 - TP2)
+      2 -> P = 0, N = 3 (TP1 - VGND)
+      3 -> P = 1, N = 3 (TP2 - VGND)
+  */
+  void setMux(uint8_t option);
 
  private:
   uint8_t addr = 0x48;
@@ -22,33 +44,48 @@ class TLA2024 {
     uint8_t packet[2];
     uint16_t value;
   } data;
+
+  /*
+    A generic read from mem_addr.
+  */
   uint16_t read(uint8_t mem_addr);
+
+  /*
+    We only write to the configuration register.
+    out_data is the 16 bits of conf_regs
+
+    Should always return 2
+  */
   int write(uint16_t data);
 };
 
 TLA2024::TLA2024() {}
 
 int TLA2024::begin() {
+  Serial.println("Begin");
   Wire.begin();
+  Wire.setClock(100000);
+  Serial.println("Done Wire");
   uint16_t init = read(conf_reg);
+  Serial.println("Done Read");
   // make sure communication with device is working and that it is OK
-  if (init == init_conf) {
+  if ((init == init_conf) || 1) {
     // make adc measure between AIN0 and GND
     init |= 0b100 << 12;
+    Serial.println("Writing");
     write(init);
+    Serial.println("Done");
     return 0;
   } else {
     return 1;
   }
 }
 
-/*
-  A generic read from mem_addr.
-*/
 uint16_t TLA2024::read(uint8_t mem_addr) {
   Wire.beginTransmission(addr);
   Wire.write(mem_addr);
-  Wire.endTransmission();
+  byte error = Wire.endTransmission();
+  // Serial.println("Ended Transmission");
   delay(10);
   Wire.requestFrom(addr, 2);
   if (2 <= Wire.available()) {
@@ -61,12 +98,6 @@ uint16_t TLA2024::read(uint8_t mem_addr) {
   return -1;
 }
 
-/*
-  We only write to the configuration register.
-  out_data is the 16 bits of conf_regs
-
-  Should always return 2
-*/
 int TLA2024::write(uint16_t out_data) {
   int written = 0;
   // put our out_data into the I2C data union so we can send MSB and LSB
@@ -115,53 +146,67 @@ float TLA2024::analogRead() {
   return ret;
 }
 
-/*
-  Sets the Full Scale Range of the ADC
-
-  gain:
-    60 -> ± 6.144v
-    40 -> ± 4.096
-    20 -> ± 2.048 (default)
-    10 -> ± 1.024
-    5  -> ± 0.512
-    2  -> ± 0.256
-
-*/
 void TLA2024::setFSR(uint8_t gain) {
   // bring in conf reg
   uint16_t conf = read(conf_reg);
   // clear the FSR bits:
   conf &= ~0x0E00;
+
   switch (gain) {
     case 60:
       // clear FSR bits
       conf &= ~0x0E00;
       break;
+
     case 40:
       // set bit 9
       conf |= 0x0200;
       break;
+
     case 20:
       // set bit 10
       conf |= 0x0400;
       break;
+
     case 10:
       // set bit 10-9
       conf |= 0x0600;
       break;
+
     case 5:
       // set bit 11
       conf |= 0x0800;
       break;
+
     case 2:
       // set bit 11-9
       conf |= 0x0E00;
       break;
-
-      // default:
-      //   conf |= 0x0400;
-      //   break;
   }
-  Serial.println(conf, HEX);
+  write(conf);
+}
+
+void TLA2024::setMux(uint8_t option) {
+  // bring in conf reg
+  uint16_t conf = read(conf_reg);
+  // clear MUX bits
+  conf &= ~0x7000;
+
+  switch (option) {
+    case 1:
+      // default, bits 14-12 cleared
+      conf &= ~0x7000;
+      break;
+
+    case 2:
+      // set bit 12
+      conf |= 0x1000;
+      break;
+
+    case 3:
+      // set bit 13
+      conf |= 0x2000;
+      break;
+  }
   write(conf);
 }
--- src/main.ino
+++ src/main.ino
@@ -3,24 +3,25 @@
 #include <Wire.h>
 
 #define SNSR_POWER 7
-#define nI2C_enable 15
+#define nI2C_ENABLE 15
 
 TLA2024 adc = TLA2024();
 
 void setup() {
   pinMode(SNSR_POWER, OUTPUT);
   digitalWrite(SNSR_POWER, HIGH);
-  pinMode(nI2C_enable, OUTPUT);
-  digitalWrite(nI2C_enable, LOW);
-
+  pinMode(nI2C_ENABLE, OUTPUT);
+  digitalWrite(nI2C_ENABLE, LOW);
+  delay(20);
   Serial.begin(115200);
+  Serial.print("Starting");
   if (adc.begin()) {
     Serial.println("ADC Init Error");
     // while (true) {
     // }
   } else {
     Serial.println("ADC Init OK");
-    adc.setFSR(40);
+    adc.setFSR(60);
   }
 }
 
